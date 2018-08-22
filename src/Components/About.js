f6234bd9ee9f4ae9a027412cce06ccf78bb9d701
Mikel Loidi <mikel.loidi@outlook.com>
2018-08-22 23:15:06 +0200

    change about and title

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,112 +1,25 @@
 import React from "react";
-import { array, string, func } from "prop-types";
-import { fetchPopularRepos } from "../utils/api";
-import Loading from "./Loading";
-
-function SelectLanguage(props) {
-  var languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
-  return (
-    <ul className="languages">
-      {languages.map(function(lang) {
-        return (
-          <li
-            style={
-              lang === props.selectedLanguage ? { color: "#d0021b" } : null
-            }
-            onClick={props.onSelect.bind(null, lang)}
-            key={lang}
-          >
-            {lang}
-          </li>
-        );
-      })}
-    </ul>
-  );
-}
-
-function RepoGrid(props) {
-  return (
-    <ul className="Popular-list">
-      {props.repos.map(function(repo, index) {
-        return (
-          <li key={repo.name} className="Popular-item">
-            <div className="Popular-rank">#{index + 1}</div>
-            <ul className="space-list-items">
-              <li>
-                <img
-                  className="avatar"
-                  src={repo.owner.avatar_url}
-                  alt={"Avatar for " + repo.owner.login}
-                />
-              </li>
-              <li>
-                <a href={repo.html_url}>{repo.name}</a>
-              </li>
-              <li>@{repo.owner.login}</li>
-              <li>{repo.stargazers_count} stars</li>
-            </ul>
-          </li>
-        );
-      })}
-    </ul>
-  );
-}
-
-RepoGrid.propTypes = {
-  repos: array.isRequired
-};
-
-SelectLanguage.propTypes = {
-  selectedLanguage: string.isRequired,
-  onSelect: func.isRequired
-};
+import { View, Mask, Container } from "mdbreact";
 
 class About extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {
-      selectedLanguage: "All",
-      repos: null
-    };
 
-    this.updateLanguage = this.updateLanguage.bind(this);
-  }
   componentDidMount() {
-    this.updateLanguage(this.state.selectedLanguage);
     const name = this.props.location.pathname;
-    this.props.updateActive(name.substr(name.indexOf("/")+1).toLowerCase());
+    this.props.updateActive(name.substr(name.indexOf("/") + 1).toLowerCase());
   }
-  updateLanguage(lang) {
-    this.setState(function() {
-      return {
-        selectedLanguage: lang,
-        repos: null
-      };
-    });
 
-    fetchPopularRepos(lang).then(
-      function(repos) {
-        this.setState(function() {
-          return {
-            repos: repos
-          };
-        });
-      }.bind(this)
-    );
-  }
   render() {
     return (
-      <div>
-        <SelectLanguage
-          selectedLanguage={this.state.selectedLanguage}
-          onSelect={this.updateLanguage}
-        />
-        {!this.state.repos ? (
-          <Loading />
-        ) : (
-          <RepoGrid repos={this.state.repos} />
-        )}
-      </div>
+      <Container>
+        <View>
+          <img
+            className="d-block w-100"
+            src="./images/nmayes.png"
+            alt="First slide"
+          />
+          <Mask overlay="black-light" />
+        </View>
+      </Container>
     );
   }
 }
